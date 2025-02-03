abb7da0cf78904d4d1eb79e0136eaaf4b1939384
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-02-03 17:40:39 +0100

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBeltComponent } from './nav-belt/nav-belt.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBeltComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/nav-belt/nav-belt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-belt',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-belt.component.html',
+  styleUrl: './nav-belt.component.css'
+})
+export class NavBeltComponent {
+
+}
