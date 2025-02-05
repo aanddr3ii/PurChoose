418489ec14c3c84c4ad774e9bfe8ead450be191c
opena00 <128498504+opena00@users.noreply.github.com>
2025-02-05 16:05:29 +0100

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: IndexComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent }, 
+
 ];
