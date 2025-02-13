686fa76043a58f66686d9cb132465d5dd698ac13
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-02-13 20:06:58 +0100

    carru

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -10,12 +10,11 @@ export class CarouselComponent {
   images = [
     { id: 1, src: 'https://m.media-amazon.com/images/I/81fkcBjZndL._AC_SX679_.jpg' },
     { id: 2, src: 'https://m.media-amazon.com/images/I/61Z9q1+EINL._AC_UL320_.jpg' },
-    { id: 3, src: 'https://media.flixcar.com/webp/synd-asset/Asus-99809225-vbix8bxyatkzctly_setting_xxx_0_90_end_1000-zoom.png' },
+    { id: 3, src: 'https://m.media-amazon.com/images/I/81mQU3If8xL._AC_SX679_.jpg' },
     { id: 4, src: 'https://m.media-amazon.com/images/I/41lixdqY0EL.__AC_SY300_SX300_QL70_ML2_.jpg' },
     { id: 5, src: 'https://m.media-amazon.com/images/I/71+4c3udwZL._AC_UL320_.jpg' },
     { id: 6, src: 'https://m.media-amazon.com/images/I/51Pc192A7rL._AC_SX679_.jpg' },
     { id: 7, src: 'https://m.media-amazon.com/images/I/71iNwni9TsL._AC_UL320_.jpg' },
-
     { id: 8, src: 'https://m.media-amazon.com/images/I/81DWnUaGM1L._AC_UL320_.jpg' },
     { id: 9, src: 'https://m.media-amazon.com/images/I/71MQ73XNbML._AC_UL320_.jpg' },
     { id: 10, src: 'https://m.media-amazon.com/images/I/515dcJmQ+iL._AC_UL320_.jpg' },
