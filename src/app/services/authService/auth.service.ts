c925f709effc5206c970c35f6ec1a4411e8362c3
RaulSeguraIbanez <128618606+RaulSeguraIbanez@users.noreply.github.com>
2025-02-21 20:13:01 +0100

    Update auth.service.ts

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {catchError, map, Observable, throwError} from "rxjs";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {User} from '../../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
