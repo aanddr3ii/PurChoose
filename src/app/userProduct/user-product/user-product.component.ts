e39150774f9c4669dc80f3e6935d1c49503a5501
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-03-09 13:16:15 +0100

    b
    b

--- src/app/userProduct/user-product/user-product.component.ts
+++ src/app/userProduct/user-product/user-product.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import { ProductFooterComponent } from '../product-footer/product-footer.component';
-
+import { NavBeltComponent } from "../../nav-belt/nav-belt.component";
+import { NavCategoriesComponent } from "../../nav-categories/nav-categories.component";
+import { ProductUserBarComponent } from "../product-user-bar/product-user-bar.component";
+import { ProductCarouselComponent } from "../product-carousel/product-carousel.component";
 import { ProductDescriptionComponent } from '../product-description/product-description.component';
 import { ProductDeliveryComponent } from '../product-delivery/product-delivery.component';
 import { ProductRefundComponent } from '../product-refund/product-refund.component';
-import { ProductUserBarComponent } from "../product-user-bar/product-user-bar.component";
-import { ProductCarouselComponent } from "../product-carousel/product-carousel.component";
-import { NavBeltComponent } from "../../nav-belt/nav-belt.component";
-import { NavCategoriesComponent } from "../../nav-categories/nav-categories.component";
 import { ProductLocationComponent } from "../product-location/product-location.component";
+import { UsersReviewComponent } from "../users-review/users-review.component";
+
+import { ProductFooterComponent } from '../product-footer/product-footer.component';
 
 @Component({
   selector: 'app-user-product',
   standalone: true,
-  imports: [ProductFooterComponent, ProductDeliveryComponent, ProductRefundComponent, ProductDescriptionComponent, ProductUserBarComponent, ProductCarouselComponent, NavBeltComponent, NavCategoriesComponent, ProductLocationComponent],
+  imports: [ProductFooterComponent, ProductDeliveryComponent, ProductRefundComponent, ProductDescriptionComponent, ProductUserBarComponent, ProductCarouselComponent, NavBeltComponent, NavCategoriesComponent, ProductLocationComponent, UsersReviewComponent],
   templateUrl: './user-product.component.html',
   styleUrl: './user-product.component.css'
 })
