221c7c8923f4915c7d39a5de74407dd9756d8a7d
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-03-10 19:24:38 +0100

    regalito para el aleix
    te le dedico 💢

--- src/app/userProduct/users-review/users-review.component.ts
+++ src/app/userProduct/users-review/users-review.component.ts
@@ -10,14 +10,14 @@ import { Review } from '../../interfaces/review';
   styleUrl: './users-review.component.css'
 })
 export class UsersReviewComponent {
-  user: User = {
-    id: 1,
-    name: 'Paco'
-  };
 
-  review: Review = {
-    id: 1,
-    rating: 3.4,
-    text: 'Excelente vendedor! Gracias'
-  };
+  users: User[] = [
+    {id:1, name: 'Aleix', profilePicture: 'https://media.licdn.com/dms/image/v2/D4D03AQHZUtDMwvW49w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1696004719195?e=2147483647&v=beta&t=zYv2c3HUsY1n9gSlk-ll0MoWictwpiixi-5nEce20H0'},
+    {id:2, name: 'Juan', profilePicture: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/002.png'}
+  ];
+  reviews: (Review & { user?: User })[] = [
+    { id: 1, rating: 5.8, text: 'Great service!', user: this.users[0] },
+    { id: 2, rating: 3.0, text: 'It was okay.', user: this.users[1] }
+  ];
+
 }
