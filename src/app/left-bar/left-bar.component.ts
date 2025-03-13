728ef1c05c54b985f500a7ea0a000e2cd0c90b93
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-03-13 20:02:40 +0100

    s
    s

--- src/app/left-bar/left-bar.component.ts
+++ src/app/left-bar/left-bar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-left-bar',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './left-bar.component.html',
   styleUrl: './left-bar.component.css'
 })
 export class LeftBarComponent {
+  isSidenavOpen = true;
 
+  toggleSidenav(event: Event) {
+    event.preventDefault(); // previene que cargue la pg acutal
+    this.isSidenavOpen = !this.isSidenavOpen;
+
+    if (this.isSidenavOpen) {
+        document.body.classList.add('no-scroll'); // no puedes hacer scroll
+    } else {
+        document.body.classList.remove('no-scroll'); // vuelves a poder hacer scroll
+    }
+  }
 }
