29cfc018cf4981ba21f57d525da6da71e4bbe802
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-03-13 17:53:08 +0100

    d
    d

--- src/app/userProduct/users-review/users-review.component.ts
+++ src/app/userProduct/users-review/users-review.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { User } from '../../interfaces/user';
-import { Review } from '../../interfaces/review';
+import { UserService } from '../../services/user/user.service';
+import { UserReviewService } from '../../services/userReview/user-review.service';
 
 @Component({
   selector: 'app-users-review',
@@ -10,14 +10,13 @@ import { Review } from '../../interfaces/review';
   styleUrl: './users-review.component.css'
 })
 export class UsersReviewComponent {
+  user: any[] = [];
+  reviews: any[] = [];
 
-  users: User[] = [
-    {id:1, name: 'Aleix', profilePicture: 'https://media.licdn.com/dms/image/v2/D4D03AQHZUtDMwvW49w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1696004719195?e=2147483647&v=beta&t=zYv2c3HUsY1n9gSlk-ll0MoWictwpiixi-5nEce20H0'},
-    {id:2, name: 'Juan', profilePicture: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/002.png'}
-  ];
-  reviews: (Review & { user?: User })[] = [
-    { id: 1, rating: 5.8, text: 'Gracias crack por el 5.8 a la proxima review negativa pal linkedin', user: this.users[0] },
-    { id: 2, rating: 3.0, text: 'It was okay.', user: this.users[1] }
-  ];
+  constructor(private userService: UserService, private userReviewService: UserReviewService) {}
 
+  ngOnInit(): void {
+    this.user = this.userService.getUser(); 
+    this.reviews = this.userReviewService.getReviews();
+  }
 }
