ab8fffc3f9ad8b59abceb8d1ee3322fa111931a8
aanddr3ii <161072980+aanddr3ii@users.noreply.github.com>
2025-03-24 19:17:41 +0100

    n
    n

--- src/app/shopping-products/shopping-box/shopping-box.component.ts
+++ src/app/shopping-products/shopping-box/shopping-box.component.ts
@@ -91,6 +91,85 @@ export class ShoppingBoxComponent {
     ],
     popularity: 4,
     dateAdded: new Date('2011-09-25')
+  },
+  
+  {
+    id: 6,
+    title: 'VOLKSWAGEN GOLF GTI 2.0 230cv 2015',
+    price: 18400,
+    location: 'Leganés',
+    description: 'FINANCIAMOS SOLO CON TU NÓMINA Y DNI ¡¡SIN ENTRADA!! POR TAN SOLO 240€ / MES',
+    images: [
+      'images/wgg2.1.png',
+      'images/wgg2.0.png',
+      'images/wgg2.3.png',
+      'images/wgg2.2.png'
+    ],
+    popularity: 2, // Popularidad (1-5)
+    dateAdded: new Date('2004-10-01') // Fecha de publicación
+  },
+  {
+    id: 7,
+    title: 'CUPRA LEON 1.5 eTSI 150CV',
+    price: 23500,
+    location: 'Leganés',
+    description: 'Coche de noviembre 2022 en perfecto estado, duerme en garaje, 19.000km',
+    images: [
+      'images/wgg2.1.png',
+      'images/wgg2.0.png',
+      'images/wgg2.3.png',
+      'images/wgg2.2.png'
+    ],
+    popularity: 4,
+    dateAdded: new Date('2011-09-25')
+  },
+  
+  {
+    id: 8,
+    title: 'CUPRA LEON 1.5 eTSI 150CV',
+    price: 23500,
+    location: 'Leganés',
+    description: 'Coche de noviembre 2022 en perfecto estado, duerme en garaje, 19.000km',
+    images: [
+      'images/wgg2.1.png',
+      'images/wgg2.0.png',
+      'images/wgg2.3.png',
+      'images/wgg2.2.png'
+    ],
+    popularity: 4,
+    dateAdded: new Date('2011-09-25')
+  },
+
+  {
+    id: 9,
+    title: 'CUPRA LEON 1.5 eTSI 150CV',
+    price: 23500,
+    location: 'Leganés',
+    description: 'Coche de noviembre 2022 en perfecto estado, duerme en garaje, 19.000km',
+    images: [
+      'images/wgg2.1.png',
+      'images/wgg2.0.png',
+      'images/wgg2.3.png',
+      'images/wgg2.2.png'
+    ],
+    popularity: 4,
+    dateAdded: new Date('2011-09-25')
+  },
+
+  {
+    id: 10,
+    title: 'CUPRA LEON 1.5 eTSI 150CV',
+    price: 23500,
+    location: 'Leganés',
+    description: 'Coche de noviembre 2022 en perfecto estado, duerme en garaje, 19.000km',
+    images: [
+      'images/wgg2.1.png',
+      'images/wgg2.0.png',
+      'images/wgg2.3.png',
+      'images/wgg2.2.png'
+    ],
+    popularity: 4,
+    dateAdded: new Date('2011-09-25')
   }
 
   ];
